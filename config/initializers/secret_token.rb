f3883754fb28d9b133b7939c2cdcc09660df70e4
C-B-Fairchild <chris.fairchild@gmail.com>
2013-09-04 23:01:53 -0400

    add all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VertiTest::Application.config.secret_key_base = '6a18e7ed514c70346ec8436ed9e89b6b3c88a0d0918e9f81396f4e107a9180427b6949721044ff2c3f7d61d658cf240cd222db2a7c44cb98b9e0f6e10a0f9994'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VertiTest::Application.config.session_store :cookie_store, key: '_verti_test_session'
